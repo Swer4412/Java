8b90f486d8f0730987d5c87d8474d71c5b37a30e
Swer4412 <swer4412@gmail.com>
2024-07-20 17:21:09 +0200

    push per arsh

--- visite_mediche/src/main/java/com/example/demo/jwt/JwtTokenUtil.java
+++ visite_mediche/src/main/java/com/example/demo/jwt/JwtTokenUtil.java
@@ -11,13 +11,15 @@ import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 
+//JwtTokenUtil è una classe che si occupa di gestire il token in se
+//Offre funzioni per generalo, 
 @Component
 public class JwtTokenUtil {
     
     public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
     
-    @Value("${jwt.secret}")
-    private String secret = "ilTuoSecretMoltoLungoEComplesso123!@#";
+    @Value("${jwt.secret}") //Dato che non me lo prende da application.properties, devo scriverlo dentro il codice.
+    private String secret = "ilTuoSecretMoltoLungoEComplesso123!@#"; 
 
     public String generateToken(String email, String scopes) {
         Map<String, Object> claims = new HashMap<>();
@@ -25,32 +27,21 @@ public class JwtTokenUtil {
         return doGenerateToken(claims, email);
     }
 
-    public Jws<Claims> parseClaimsFromToken(String token) {
-        return Jwts.parser().verifyWith(Keys.hmacShaKeyFor(this.secret.getBytes())).build().parseSignedClaims(token);
-    }
-
     private String doGenerateToken(Map<String, Object> claims, String subject) {
         return Jwts.builder()
-                .id(UUID.randomUUID().toString())
-                .subject(subject)
-                .issuer("api-comuni")
-                .issuedAt(new Date(System.currentTimeMillis()))
-                .setAudience("http://localhost:8080")
-                .claims(claims)
-                .expiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))
-                .signWith(Keys.hmacShaKeyFor(this.secret.getBytes()))
-            .compact();
+            .id(UUID.randomUUID().toString())  // Identificatore unico del token
+            .subject(subject)  // Identificativo dell'utente, tipicamente username o email
+            .issuer("api-comuni")  // Chi ha emesso il token
+            .issuedAt(new Date(System.currentTimeMillis()))  // Quando è stato emesso il token
+            .setAudience("http://localhost:8080")  // A chi è destinato il token
+            .expiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))  // Quando scadrà il token
+            .claims(claims)  // Claims aggiuntivi personalizzati
+            .signWith(Keys.hmacShaKeyFor(this.secret.getBytes()))  // Firma del token
+            .compact();  // Costruisce il token finale
     }
-
-    public Boolean isTokenExpired(Jws<Claims> jws) {
-        final Date expiration = getExpirationDateFromToken(jws);
-        return expiration.before(new Date());
-    }
-
-    private Date getExpirationDateFromToken(Jws<Claims> jws) {
-        return jws.getPayload().getExpiration();
-    }
-
+    
+    //FUNZIONI HELPER
+    
     public boolean validateToken(String token) {
         try {
             Jws<Claims> claims = parseClaimsFromToken(token);
@@ -60,8 +51,23 @@ public class JwtTokenUtil {
         }
     }
 
+    //Serve per estrarre l'email dal corpo del token, in poche parole per ottenere l'identità
     public String getEmailFromToken(String token) {
-        Jws<Claims> claims = parseClaimsFromToken(token);
-        return claims.getBody().getSubject();
+        Jws<Claims> jws = parseClaimsFromToken(token);
+        return jws.getPayload().getSubject();
+    }
+    
+    //Serve per vedere se il token é scaduto dopo 5 ore (JWT_TOKEN_VALIDITY)
+    public Boolean isTokenExpired(Jws<Claims> jws) {
+        final Date expiration = getExpirationDateFromToken(jws);
+        return expiration.before(new Date());
+    }
+    
+    private Date getExpirationDateFromToken(Jws<Claims> jws) {
+        return jws.getPayload().getExpiration();
+    }
+    
+    public Jws<Claims> parseClaimsFromToken(String token) {
+        return Jwts.parser().verifyWith(Keys.hmacShaKeyFor(this.secret.getBytes())).build().parseSignedClaims(token);
     }
 }
